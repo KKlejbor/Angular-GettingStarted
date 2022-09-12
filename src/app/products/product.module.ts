d226ec829284b0d8ca70011ae0306b1173c3873d
Krzysztof Klejbor <krzysztof.klejbor@gmail.com>
2022-09-12 16:13:48 +0200

    Export product related features to product.module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,10 +1,31 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { ConvertToSpacesPipe } from "../shared/convert-to-spaces.pipe";
+import { StarComponent } from "../shared/star.component";
+import { ProductDetailComponent } from "./product-detail.component";
+import { ProductDetailGuard } from "./product-detail.guard";
+import { ProductListComponent } from "./product-list.component";
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        ProductListComponent,
+        ProductDetailComponent,
+        ConvertToSpacesPipe,
+        StarComponent
+    ],
     imports: [
-        CommonModule
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([
+            {path: "products", component: ProductListComponent},
+            {
+                path: "products/:id",
+                canActivate: [ProductDetailGuard],
+                component: ProductDetailComponent
+            }
+        ])
     ]
 })
 export class ProductModule{}
