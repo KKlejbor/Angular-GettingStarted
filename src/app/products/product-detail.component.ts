f5f452ed399e6c97b05ef539cf8fa03ce2d0810a
Krzysztof Klejbor <krzysztof.klejbor@gmail.com>
2022-09-12 15:05:38 +0200

    Add product fetching for product-detail.component
    The component will use the product service to fetch all products
    
    Then it will search for a specific product using the find method

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,21 +1,34 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     templateUrl: "./product-detail.component.html",
     styleUrls: ["./product-detail.component.css"]
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
     pageTitle: string = "Product Detail";
     product: IProduct | undefined;
+    subscritpion!: Subscription;
+    errorMessage: string ="";
     
     constructor(private route: ActivatedRoute,
-                private router: Router) {}
+                private router: Router,
+                private productService: ProductService) {}
 
     ngOnInit(): void{
         const id = Number(this.route.snapshot.paramMap.get("id"));
-        this.pageTitle += `: ${id}`;
+        
+        this.subscritpion = this.productService.getProducts().subscribe({
+            next: products => this.product = products.find((element) => element.productId === id),
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.subscritpion.unsubscribe();
     }
 
     onBack(): void{
